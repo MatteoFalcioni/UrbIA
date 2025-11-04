b1341c7b4177e124c15e6f3a7897fd2f80c837a0
MatteoFalcioni <matteo.falk@gmail.com>
2025-11-04 16:13:55 +0100

    (!) updated state

--- backend/graph/state.py
+++ backend/graph/state.py
@@ -21,7 +21,69 @@ def update_token_count(token_count: int | None = None, token_used: int | None = 
     else:
         return token_count + token_used
 
+def merge_dicts(
+    left: dict[str, str] | None = None,
+    right: dict[str, str] | None = None
+) -> dict[str, str]:
+    """Merge two dictionaries. Left takes precedence over right. Used for reports."""
+    if left is None:
+        left = {}
+    if right is None:
+        right = {}
+    return {**left, **right}
+
+def merge_dicts_nested(
+    left: dict[str, dict[str, str]] | None = None, 
+    right: dict[str, dict[str, str]] | None = None
+) -> dict[str, dict[str, str]]:
+    """Merge two nested dictionaries. Left takes precedence over right. Used for sources."""
+    if left is None:
+        left = {}
+    if right is None:
+        right = {}
+    return {**left, **right}
+
+def list_add(
+    left: list[dict[str, str]] | None = None,
+    right: list[dict[str, str]] | None = None
+) -> list[dict[str, str]]:
+    """Add a new item to a list. Used for code. No deduplication - running the same code twice is meaningful."""
+    if left is None:
+        left = []
+    if right is None:
+        right = []
+    
+    return left + right
+
+def str_replace(
+    left: str | None,
+    right: str | None
+) -> str:
+    """Update a string just by replacing it. Reducer needed to initialize when None"""
+    if left is None:
+        return ""
+    else:
+        return right
+
+def bool_replace(
+    left: bool | None,
+    right: bool | None
+) -> bool:
+    """Update a boolean just by replacing it. Reducer needed to initialize when None"""
+    if left is None:
+        return False
+    else:
+        return right
+
 
 class MyState(AgentState):
+    # summary and token count features (core)
     summary : str   # No reducer - just replace
     token_count : Annotated[int, update_token_count]
+    # write report features 
+    sources : Annotated[dict[str, dict[str, str]], merge_dicts_nested] # key is the dataset id, value is a dict with desc, url
+    reports: Annotated[dict[str, str], merge_dicts]  # key is the title, value is the content 
+    write_report : Annotated[bool, bool_replace]
+    last_report_title : Annotated[str, str_replace]
+    edit_instructions : Annotated[str, str_replace]
+    code_logs: Annotated[list[dict[str, str]], list_add]  # list of dicts (we need chronologcal order!), each dicts is input and output of a code block (out can be stdout or stderr or both)
