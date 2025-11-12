11f232b984ba2854e2d28a940a3e80c8de2c396e
MatteoFalcioni <matteo.falk@gmail.com>
2025-11-12 11:19:16 +0100

    (!!) changed supervisor tools - no .PARENT

--- backend/graph/tools/supervisor_tools.py
+++ backend/graph/tools/supervisor_tools.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from langchain.tools import tool, ToolRuntime
-from langchain_core.messages import ToolMessage
+from langchain_core.messages import ToolMessage, HumanMessage
 from langgraph.types import Command
 
 
@@ -11,27 +11,27 @@ def create_handoff_tool(*, agent_name: str, description: str | None = None):    
 
     # the actual handoff tool
     @tool(name, description=description)
-    def handoff_tool(runtime : ToolRuntime) -> Command:
+    def handoff_tool(task: Annotated[str, "The task that the subagent should perform"], runtime : ToolRuntime) -> Command:
 
         tool_msg = ToolMessage(content=f"Successfully transferred to {agent_name}", tool_call_id=runtime.tool_call_id)
+        task_msg = HumanMessage(content=f"The agent supervisor adivces you to perform the following task : \n{task}")
         state = runtime.state
         
         return Command(
             goto=agent_name,  
-            update={**state, "messages": state["messages"] + [tool_msg]},  
-            graph=Command.PARENT,  
+            update={**state, "messages": state["messages"] + [tool_msg] + [task_msg]},   
         )
 
     return handoff_tool
 
 # Handoffs
-assign_to_analyst_agent = create_handoff_tool(
-    agent_name="data analyst",
+assign_to_analyst = create_handoff_tool(
+    agent_name="data_analyst",
     description="Assign task to the data analyst agent.",
 )
 
 assign_to_report_writer = create_handoff_tool(
-    agent_name="report writer",
+    agent_name="report_writer",
     description="Assign task to the report writer agent.",
 )
 
